5926f7d96c6e6805706ca87b540092ba197d2eeb
AVATAR ID <68086971+romli2020@users.noreply.github.com>
2020-11-09 17:40:58 +0700

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -3,6 +3,7 @@
 
 clear
 read -p "Udah Subscribe Cannel Avatar ID ? : " nama;
+clear
 echo -e "\033[31;1m+\033[33;1m-----------------------------------------\033[0;32m+"
 echo -e "\033[0;32m[+] Author   : \033[0;36mRomli"
 echo -e "\033[0;32m[+] Youtube  : \033[0;36mAVATAR ID"
